8c5e0816528bbdb1d91821329bf6708edce30f80
ariyan <ariyanbolandi3@gmail.com>
2025-03-04 23:48:57 +0330

    camel.py has Added

---
+++ practices/camel.py
@@ -0,0 +1,15 @@
+import re
+
+
+def convert_case(s):
+    if "_" in s:
+        parts = s.split("_")
+        return parts[0] + "".join(word.capitalize() for word in parts[1:])
+    else:
+
+        return re.sub(r'([a-z])([A-Z])', r'\1_\2', s).lower()
+
+
+user_input = input("Enter a string in camelCase or snake_case: ")
+converted = convert_case(user_input)
+print("Converted:", converted)
--- practices/extensions.py
+++ practices/extensions.py
@@ -1,19 +1,28 @@
+from email.policy import default
 from itertools import chain
 
 def check_format(file_format, valid_extensions, formats):
     if len(file_format) == 2 and file_format[1] in valid_extensions:
         for keys, values in formats.items():
             if file_format[1] in values:
-                print(f"{keys}/{file_format[1]}")
+                if file_format[1] == "jpg":
+                    file_format[1] = "jpeg"
+                if file_format[1] == "txt":
+                    print("text/plain")
+                print(f"{keys}/{file_format[1]}",end="")
 
 def main():
-    formats = {"application": ["pdf", "text", "zip"],
-               "image": ["jpg", "gif", "jpeg"]}
+    formats = {"application": ["pdf", "zip","octet-stream"],
+               "image": ["jpg", "gif", "jpeg","png"],
+               }
 
-    user_file_format = input("File name:").rsplit('.', 1)
-
+    user_file_format = input("File name:").lower().strip().rsplit('.', 1)
+    if "bin" in user_file_format or "" in user_file_format:
+        user_file_format.pop.append("octet-stream")
+    print(user_file_format)
     valid_extensions = list(chain(*formats.values()))
     check_format(user_file_format, valid_extensions, formats)
+
 
 if __name__ == "__main__":
     main()
