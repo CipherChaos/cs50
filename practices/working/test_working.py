b3874953304919ec634068822a0784f08f7a0d6f
ariyan <ariyanbolandi3@gmail.com>
2025-04-07 23:02:49 +0330

    (Working) File reviewed again

--- practices/working/test_working.py
+++ practices/working/test_working.py
@@ -2,24 +2,26 @@ import pytest
 from practices.working.working import convert
 
 
-
 def main():
     test_time()
+    test_time_format()
+
 
 def test_time():
-    assert convert(r"9 AM to 5 PM") == "09:00 to 17:00"
-    assert convert(r"9:00 AM to 5:00 PM") == "09:00 to 17:00"
-    assert convert(r"10 AM to 8:50 PM") == "10:00 to 20:50"
-    assert convert(r"10:30 PM to 8 AM") == "22:30 to 08:00"
-    assert convert(r"9 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
+    assert convert("10 AM to 8:50 PM") == "10:00 to 20:50"
+    assert convert("10:30 PM to 8 AM") == "22:30 to 08:00"
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
 
+
+def test_time_format():
     with pytest.raises(ValueError):
         convert("9:60 AM to 5:60 PM")
     with pytest.raises(ValueError):
-        convert("9 AM - 5 PM,")
+        convert("9 AM - 5 PM")
     with pytest.raises(ValueError):
         convert("09:00 AM - 17:00 PM")
-
 
 
 if __name__ == "__main__":
--- practices/working/working.py
+++ practices/working/working.py
@@ -1,28 +1,51 @@
 import re
-import sys
 
 
 def main():
     print(convert(input("Hours: ")))
 
 
-def convert(s):
-    time_pattern = r"^(\d{1,2}:?\d{2})? (A|P\M) to$"
-    match = re.search(time_pattern, s)
-    if match:
-        return match.group()
-    else:
-        return None
+def convert_hours(hh, mm, ss):
+    hours = int(hh)
+    if ss == "PM":
 
-    """
-    
-        9 AM to 5 PM
-        9:00 AM to 5:00 PM
-        10 AM to 8:50 PM
-        10:30 PM to 8 AM
-        9 AM to 5 PM
-    
-    """
+        if hours == 12:
+            hh_update = 12
+        else:
+            hh_update = hours + 12
+    else:
+        if hours == 12:
+            hh_update = 0
+        else:
+            hh_update = hours
+
+    updated_time = f"{hh_update:02}" + ":" + f"{mm:02}"
+    return updated_time
+
+
+def convert(s):
+    time_pattern = r"^(\d{1,2}):?(\d{2})? (AM|PM) to (\d{1,2}):?(\d{2})? (AM|PM)$"
+    match = re.search(time_pattern, s, re.I)
+
+    if match:
+        time = list(match.groups())
+
+        if time[1] is None:
+            time[1] = 0
+
+        if time[4] is None:
+            time[4] = 0
+        if int(time[0]) > 12 or int(time[3]) > 12:
+            raise ValueError
+        if int(time[1]) > 59 or int(time[4]) > 59:
+            raise ValueError
+
+        left_clock = convert_hours(time[0], time[1], time[2])
+        right_clock = convert_hours(time[3], time[4], time[5])
+
+        return left_clock + " to " + right_clock
+    else:
+        raise ValueError
 
 
 if __name__ == "__main__":
